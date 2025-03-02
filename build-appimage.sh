07a43558bb749a1fd368d363ffd6b93bbfc60022
Martin Dorazil - LINUX <mmdorazil@gmail.com>
2025-03-02 19:34:29 +0100

    Github workflow

---
+++ build-appimage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+blc -build --release
+echo Creating application bundle
+rm -r -f Tine.AppDir
+mkdir -p Tine.AppDir/usr/bin
+mkdir -p Tine.AppDir/usr/lib
+mkdir -p Tine.AppDir/usr/share/applications
+mkdir -p Tine.AppDir/usr/share/icons/hicolor/256x256/apps
+
+cp Tine Tine.AppDir/usr/bin/
+cp icon/icon_256x256.png Tine.AppDir/usr/share/icons/hicolor/256x256/apps/Tine.png
+echo -e "[Desktop Entry]\nName=Tine\nExec=Tine\nIcon=Tine\nType=Application\nCategories=Utility\n" > Tine.AppDir/usr/share/applications/Tine.desktop
+./linuxdeploy-x86_64.AppImage --appdir Tine.AppDir --output appimage
+./appimagetool-x86_64.AppImage Tine.AppDir
+
