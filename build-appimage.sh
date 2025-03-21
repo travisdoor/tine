158bcbbc6d30aa6d138ade56834ae72f9d44a866
Martin Dorazil (linux) <mmdorazil@gmail.com>
2025-03-21 23:13:56 +0100

    Automatically create linux desktop entry on the first run.

--- build-appimage.sh
+++ build-appimage.sh
@@ -10,6 +10,7 @@ mkdir -p Tine.AppDir/usr/share/icons/hicolor/256x256/apps
 cp Tine Tine.AppDir/usr/bin/
 cp icon/icon_256x256.png Tine.AppDir/usr/share/icons/hicolor/256x256/apps/Tine.png
 echo -e "[Desktop Entry]\nName=Tine\nExec=Tine\nIcon=Tine\nType=Application\nCategories=Utility\n" > Tine.AppDir/usr/share/applications/Tine.desktop
+# export NO_STRIP=true
 ./linuxdeploy-x86_64.AppImage --appdir Tine.AppDir --output appimage
 ./appimagetool-x86_64.AppImage Tine.AppDir
 
